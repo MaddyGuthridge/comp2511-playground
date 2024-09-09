4be1e0e2a5dcdb42a735545e0f24c4bb01c97676
Maddy Guthridge <hello@maddyguthridge.com>
2024-09-09 17:06:17 +1000

    Add people example

--- app/src/main/java/splitter/Splitter.java
+++
@@ -1,8 +0,0 @@
-package splitter;
-
-public class Splitter {
-    public static void main(String[] args) {
-        System.out.println("Enter a sentence specified by spaces only:");
-        // Add your code
-    }
-}
